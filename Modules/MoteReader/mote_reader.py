6fe3a00a1d91d72d6f5d3d712eee9d0209598d99
Jamie Finney <jfinney10@gmail.com>
2013-11-19 17:19:11 -0500

    Updated mote_reader.py

--- Modules/MoteReader/mote_reader.py
+++ Modules/MoteReader/mote_reader.py
@@ -10,9 +10,9 @@ while 1:
   bytes_read = port.read (100)
   #print len (bytes_read)
   if len (bytes_read) == 77:
-    for i in range (0, len(bytes_read)):
-      print i
-      print hex(int(ord(bytes_read [i])))
+    #for i in range (0, len(bytes_read)):
+      #print i
+      #print hex(int(ord(bytes_read [i])))
  
 
     #num_of_readings = (int (ord (bytes_read [10])) << 8) + int (ord (bytes_read [11]))
@@ -20,23 +20,23 @@ while 1:
     #print hex(int(ord(bytes_read [10])))
     #print hex(int(ord(bytes_read [11])))
 
-    #num_of_readings = (int (ord (bytes_read [11])) << 8) + int (ord (bytes_read [12]))
-    #print num_of_readings
+    num_of_readings = (int (ord (bytes_read [12])) << 8) + int (ord (bytes_read [13]))
+    print num_of_readings
 
-    #light_vals = []
-    #for i in range (16, 35, 2):
-      #light_vals.append ((int (ord (bytes_read [i])) << 8) + int (ord (bytes_read [i + 1])))
-    #print light_vals
+    light_vals = []
+    for i in range (14, 33, 2):
+      light_vals.append ((int (ord (bytes_read [i])) << 8) + int (ord (bytes_read [i + 1])))
+    print light_vals
 
-    #temp_vals = []
-    #for i in range (36, 54, 2):
-      #temp_vals.append ((int (ord (bytes_read [i])) << 8) + int (ord (bytes_read [i + 1])))
-    #print temp_vals
+    temp_vals = []
+    for i in range (34, 53, 2):
+      temp_vals.append ((int (ord (bytes_read [i])) << 8) + int (ord (bytes_read [i + 1])))
+    print temp_vals
 
-    #hum_vals = []
-    #for i in range (55, 76, 2):
-      #hum_vals.append ((int (ord (bytes_read [i])) << 8) + int (ord (bytes_read [i + 1])))
-    #print hum_vals
+    hum_vals = []
+    for i in range (54, 73, 2):
+      hum_vals.append ((int (ord (bytes_read [i])) << 8) + int (ord (bytes_read [i + 1])))
+    print hum_vals
     #values = struct.unpack ('H', bytes_read [0:1])
 
   time.sleep (.2)
